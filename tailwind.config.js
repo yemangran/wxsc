78b7b0fd1cbcb105c5258b22689f3f03545992cf
wangqing <tlxiaoyun@qq.com>
2024-08-21 13:34:43 +0800

    add some styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,7 @@ import daisyui from "daisyui"
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}', './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'],
-	theme: {
-		extend: {}
-	},
+	theme: {},
 	darkMode: 'class',
-	plugins: [nextui(),daisyui]
+	plugins: [nextui(), daisyui]
 }
